e7fa16546263986cbc767a5223c48ae31928ef6a
AllenNotAlan <allenloyola23@gmail.com>
2020-07-11 14:38:05 +0100

    added leetcode9 to csharp solutions

---
+++ CSharp/LeetCode9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LeetCode9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 323;
+            string numToString = x.ToString();
+            char[] stringToCharArr = numToString.ToCharArray();
+            Array.Reverse(stringToCharArr);
+
+            string reversedNumToString = new string(stringToCharArr);
+
+            if(numToString == reversedNumToString)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("Nah mate"); 
+            }
+
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            string numToString = x.ToString();
+            char[] stringToCharArr = numToString.ToCharArray();
+            Array.Reverse(stringToCharArr);
+
+            string reversedNumToString = new string(stringToCharArr);
+
+            if (numToString == reversedNumToString)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
