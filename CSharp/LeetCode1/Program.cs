e28c3847d9477ad0f0f5e03f9a8f9ae2a7d1fc3f
AllenNotAlan <allenloyola23@gmail.com>
2020-07-11 14:07:32 +0100

    Added solutions in CSharp@

---
+++ CSharp/LeetCode1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeetCode1
+{
+    class Program
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                    }
+                }
+            }
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            int[] nums = {2, 7, 11, 15};
+            int target = 9;
+
+            Program twoSum = new Program();
+
+            
+            int[] result = twoSum.TwoSum(nums, target);
+
+            for(int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/LeetCode7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program test = new Program();
+
+            int x = 123;
+            int y = -123;
+
+            int result = test.Reverse(y);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public int Reverse(int x)
+        {
+
+            int reversed = 0;
+
+            //for positive x
+            while(x > 0)
+            {
+                int pop = x % 10;
+                x /= 10;
+                if (reversed > Int32.MaxValue/10 || (reversed == Int32.MaxValue/10 && pop > 7))
+                {
+                    return 0;
+                }
+                
+                if (reversed < Int32.MinValue/10 || (reversed == Int32.MinValue/10 && pop < -8))
+                {
+                    return 0;
+                }
+                reversed = reversed * 10 + pop;
+            }
+
+            //for negative x
+            while (x < 0 )
+            {
+                int pop = x % 10;
+                x /= 10;
+                if (reversed > Int32.MaxValue / 10 || (reversed == Int32.MaxValue / 10 && pop > 7))
+                {
+                    return 0;
+                }
+
+                if (reversed < Int32.MinValue / 10 || (reversed == Int32.MinValue / 10 && pop < -8))
+                {
+                    return 0;
+                }
+                reversed = reversed * 10 + pop;
+            }
+
+            return reversed;
+
+        }
+    }
+}
